cab2ed3663de715dc506d60ddc9950f9550f20c5
Alexander Arévalo <alexander_arevalo@icloud.com>
2023-09-23 02:33:29 -0300

    Init Angular 16 with Vite.

---
+++ src/about.component.ts
@@ -0,0 +1,24 @@
+import { Component, signal, computed, effect } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+selector: 'app-about',
+standalone: true,
+imports: [CommonModule],
+template: `
+<p> hello, this is about us page </p>
+{{ fullName() }} <button (click)="setName('John')">Click</button>
+`,
+})
+export class AboutComponent {
+    firstName = signal('Jane');
+    lastName = signal('Doe');
+    fullName = computed(() => `${this.firstName()} ${this.lastName()}`);
+
+    constructor() {
+        effect(() => console.log('Name changed:', this.fullName()));
+    }
+
+    setName(newName: string) {
+        this.firstName.set(newName);
+    }
+}
