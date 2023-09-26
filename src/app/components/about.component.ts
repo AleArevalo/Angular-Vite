9d044e3fa6ee774bbc43df7306e3b9efa503681a
Alexander Arévalo <alexander_arevalo@icloud.com>
2023-09-26 20:06:15 -0300

    Clean structure files.

--- src/app/components/about.component.ts
+++ src/app/components/about.component.ts
@@ -1,14 +1,16 @@
 import { Component, signal, computed, effect } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 @Component({
-selector: 'app-about',
-standalone: true,
-imports: [CommonModule],
-template: `
-<p> hello, this is about us page </p>
-{{ fullName() }} <button (click)="setName('John')">Click</button>
-`,
+    selector: 'app-about',
+    standalone: true,
+    imports: [CommonModule],
+    template: `
+        <p> hello, this is about us page </p>
+        {{ fullName() }} <button (click)="setName('John')">Click</button>
+    `,
 })
+
 export class AboutComponent {
     firstName = signal('Jane');
     lastName = signal('Doe');
