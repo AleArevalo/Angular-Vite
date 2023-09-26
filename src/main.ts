37a64a784dad80bd3a734d2ea450488ebdf9d6a8
Alexander Arévalo <alexander_arevalo@icloud.com>
2023-09-25 22:43:43 -0300

    Update order carpets.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "@angular/compiler";
 import "zone.js";
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { appRouting } from "./app.routing";
+import { AppComponent } from './app/app.component';
+import { appRouting } from "./app/app.routing";
 bootstrapApplication(AppComponent, {providers: [...appRouting]});
